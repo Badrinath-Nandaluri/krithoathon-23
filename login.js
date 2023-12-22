c5fe5519113a6ff8b9ee292645180a0de20fe226
Badrinath-Nandaluri <badri.acads@gmail.com>
2023-12-22 22:50:08 +0530

    initial commit of files

---
+++ login.js
@@ -0,0 +1,11 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    // Implement your login logic here
+    // For example, you can send the credentials to your server for validation
+});
+
+
